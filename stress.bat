c3046c7531b8292d50ac678652f03a1f0496e09b
andruha2007 <andruha141207@gmail.com>
2026-01-11 19:44:56 +0300

    Final additions

--- stress.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set EXE=image_craft.exe
-
-echo ==== STRESS: 50 раз heavy-цепочка ====
-for /L %%i in (1,1,50) do (
-    echo Run %%i
-    %EXE% in.bmp stress_%%i.bmp -crop 1024 1024 -med 9 -blur 5 -crystallize 8 -glass 3
-)
-pause
